e69e37b520ebb6956593aad0c4603b3df658c41e
Neil Rosenstech <tohellandback@hotmail.fr>
2013-08-01 11:00:17 -0400

    Handle local commits

--- lib/the_collector.rb
+++ lib/the_collector.rb
@@ -10,9 +10,15 @@ class Collector
     def setup
       yield self
       base_uri uri
-      options = {
+      post('/', body: options)
+    end
+
+    def options
+      lookup = remote_lookup
+      {
         state: {
-          ref:           `cd #{path} && git rev-parse HEAD`.strip,
+          ref:           lookup[:ref],
+          local_commits: lookup[:local_commits],
           branch:        `cd #{path} && git rev-parse --abbrev-ref HEAD`.strip,
           local_changes: !`cd #{path} && git status -s`.strip.empty?,
           diff:          `cd #{path} && git diff`,
@@ -22,7 +28,17 @@ class Collector
         installation:      { name: installation, location: location, env: env },
         authorization_key: authorization_key
       }
-      post('/', body: options)
+    end
+
+    def remote_lookup
+      i, found = 0, false
+      until found do
+        ref =          `cd #{path} && git rev-parse HEAD~#{i}`.strip
+        check_remote = `cd #{path} && git branch -r --contains #{ref}`.strip
+        found =        check_remote.include? 'origin'
+        i += 1
+      end
+      { ref: ref, local_commits: i-1 }
     end
 
     def path
