a17beb0f68fe2190b2ecdbbd9ea05905273c124a
Maxime Handfield Lapointe <hunter_spawn@hotmail.com>
2016-12-05 10:28:48 -0500

    Improved exception handling during setup

--- lib/zecollector.rb
+++ lib/zecollector.rb
@@ -9,26 +9,28 @@ class Collector
 
     def setup!(&block)
       Thread.new(self) do |_self|
-        yield _self
-        _self.base_uri uri
         begin
+          yield _self
+          _self.base_uri uri
           raise 'Missing authorization key' unless authorization_key
+          options_to_use = self.options # No need to recalculate in each attempts and exception right away if needed.
           error_message = nil
           [0, 5, 15, 60].each do |seconds|
             sleep seconds
             begin
               error_message = nil
-              response = _self.post('/', body: options)
+              response = _self.post('/', body: options_to_use)
             rescue => e
-              # so we need to catch the exception here to loop again and retry in 5, 15 or 60 seconds
+              # so we need to catch the exception here to loop again and retry in some seconds
               # otherwise a post related timeout exception would be rescued by the parent block and no retry happens
-              error_message = e.to_s
+              message = e.to_s.encode('utf-16', :invalid => :replace, :undef => :replace).encode('utf-8')
+              error_message = "#{e.class}: #{message}\n#{e.backtrace.join("\n")}"
             else
               break if response.code == 200
               error_message = "Error #{response.code}: #{response.message}"
             end
           end
-          raise("Unable to send data to arma:\n#{error_message}") if error_message.present?
+          raise("Unable to send data to arma:\n#{error_message}") if error_message
           _self.code_changelog.commit if defined?(CodeChangelog)
         rescue => e
           _self.exception_callback.call(e)
